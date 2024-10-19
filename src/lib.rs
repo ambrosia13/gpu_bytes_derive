3af92d9672f693669e2cd8b06c78098a0fdcb416
ambrosia13 <aavyakumbhare@gmail.com>
2024-10-19 14:01:06 -0500

    implement std140 macro

--- src/lib.rs
+++ src/lib.rs
@@ -43,5 +43,46 @@ pub fn std140_derive(input: TokenStream) -> TokenStream {
     }
 }
 
+#[proc_macro_derive(AsStd430)]
+pub fn std430_derive(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = input.ident;
+
+    match input.data {
+        syn::Data::Struct(data_struct) => {
+            let write_calls = data_struct.fields.into_iter().map(|field| {
+                let field_name = field.ident;
+
+                quote! {
+                    buf.write(&self.#field_name);
+                }
+            });
+
+            quote! {
+                impl gpu_bytes::AsStd430 for #name {
+                    fn as_std140(&self) -> gpu_bytes::Std430Bytes {
+                        let mut buf = gpu_bytes::Std430Bytes::new();
+
+                        #(
+                            #write_calls
+                        )*
+
+                        buf
+                    }
+                }
+            }
+            .into()
+        }
+        syn::Data::Enum(_) => syn::Error::new_spanned(name, "AsStd430 cannot be derived for enums")
+            .to_compile_error()
+            .into(),
+        syn::Data::Union(_) => {
+            syn::Error::new_spanned(name, "AsStd430 cannot be derived for unions")
+                .to_compile_error()
+                .into()
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {}
